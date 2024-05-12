0d8456efde82f2b81b6ba9329e763967e44b5300
wenbi.cheng <1159076075@qq.com>
2024-05-12 16:48:38 +0800

    并查集.

---
+++ data-structures/src/main/java/com/cc/data/structres/unionFind/UnionFind.java
@@ -0,0 +1,27 @@
+package com.cc.data.structres.unionFind;
+
+public class UnionFind {
+    int[] parent;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+        }
+    }
+}
